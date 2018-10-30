c4a7373d2975fe45974b54e0396c45c2d7f51c58
User <user@MacBook-Pro-User.local>
2018-10-30 18:57:35 +0600

    fix:printf minInt8

--- Part 1 Basics.playground/Contents.swift
+++ Part 1 Basics.playground/Contents.swift
@@ -1,6 +1,7 @@
 var maxUInt8 = UInt8.max
 print(maxUInt8)
-//var minInt8 = Int8.min
+var minInt8 = Int8.min
+print(minInt8)
 //var dannaPeremena : String = "dannaPeremena"
 //var dannaPeremena1 = "dannaPeremena1"
 //var peremennYa = dannaPeremena1
