57f6ee1b2407327fc47c8cc33bc158fcb42ab6d3
User <user@MacBook-Pro-User.local>
2018-10-30 18:43:23 +0600

    First commit

---
+++ Part 1 Basics.playground/Contents.swift
@@ -0,0 +1,11 @@
+var maxUInt8 = UInt8.max
+var minInt8 = Int8.min
+var dannaPeremena : String = "dannaPeremena"
+var dannaPeremena1 = "dannaPeremena1"
+var peremennYa = dannaPeremena1
+dannaPeremena1 = dannaPeremena
+dannaPeremena = peremennYa
+print("Ya vivozhu \(dannaPeremena)")
+print("Ya vivozhu \(dannaPeremena1)")
+
+
