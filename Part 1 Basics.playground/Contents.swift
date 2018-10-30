b5f9dc60962e1235d5db4af02067dada95776804
User <user@MacBook-Pro-User.local>
2018-10-30 19:19:48 +0600

    fix/remove-comments

--- Part 1 Basics.playground/Contents.swift
+++ Part 1 Basics.playground/Contents.swift
@@ -1,12 +1,12 @@
 var maxUInt8 = UInt8.max
-print(maxUInt8)
-//var minInt8 = Int8.min
-//var dannaPeremena : String = "dannaPeremena"
-//var dannaPeremena1 = "dannaPeremena1"
-//var peremennYa = dannaPeremena1
-//dannaPeremena1 = dannaPeremena
-//dannaPeremena = peremennYa
-//print("Ya vivozhu \(dannaPeremena)")
-//print("Ya vivozhu \(dannaPeremena1)")
+var minInt8 = Int8.min
+var dannaPeremena : String = "dannaPeremena"
+var dannaPeremena1 = "dannaPeremena1"
+var peremennYa = dannaPeremena1
+dannaPeremena1 = dannaPeremena
+dannaPeremena = peremennYa
+print("Ya vivozhu \(dannaPeremena)")
+print("Ya vivozhu \(dannaPeremena1)")
 
-
+var bar = "Swift"
+print("I'm learning \(bar)")
